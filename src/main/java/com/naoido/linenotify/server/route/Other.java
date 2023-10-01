10d025a85ea1d27562c78d5440245d1d5e7841da
なおいど <sentyouido@gmail.com>
2023-10-01 16:15:09 +0900

    存在しないものは403ではなく404を返すように

--- src/main/java/com/naoido/linenotify/server/route/Other.java
+++ src/main/java/com/naoido/linenotify/server/route/Other.java
@@ -1,4 +1,4 @@
-package com.naoido.linenotify.api.route;
+package com.naoido.linenotify.server.route;
 
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,26 +7,26 @@ import jakarta.servlet.http.HttpServletResponse;
 public class Other extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
-        resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
     }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
-        resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
     }
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) {
-        resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
     }
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) {
-        resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
     }
     @Override
     protected void doOptions(HttpServletRequest req, HttpServletResponse resp) {
-        resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
     }
     @Override
     protected void doHead(HttpServletRequest req, HttpServletResponse resp) {
-        resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
     }
 }
